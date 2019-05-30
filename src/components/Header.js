80f80cd91126f024dd7157e742c97c25ea6fb3ed
Emily Seieroe <e.e.seieroe@gmail.com>
2019-05-29 20:47:31 -0400

    adding modal

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Dialog} from "@reach/dialog";
+import "@reach/dialog/styles.css";
+import CartContainer from '../containers/CartContainer'
 
 const Header = ({ products, total }) => {
-
+  const [ modalOpen, setModalOpen ] = useState(false)
   console.log("these are my products", products)
 
 // {/* Refactor with just the strings being conditionally rendered */}
+let buttonText;
+
   if (products.length == 0) {
-    return (
-    <div>
-      <h1> ACME STORE </h1>
-      <button> Your Cart is empty </button>
-    </div>
-  )} else {return (
-    <div>
-      <h1> ACME STORE </h1>
-      <button> Your Cart </button>
-    </div>
-  )}
+    buttonText = "Your Cart is empty" } else { buttonText = "Your Cart"}
+  return (
+  <div>
+    <h1> ACME STORE </h1>
+    <button onClick={() => setModalOpen(true)}> {buttonText} </button>
+
+    <Dialog
+       isOpen={modalOpen}
+       onDismiss={() => setModalOpen(false)}
+     >
+       <button
+         className="close-button"
+         onClick={() => setModalOpen(false)}
+       >
+         {/*<VisuallyHidden>Close</VisuallyHidden>*/}
+         <span aria-hidden>×</span>
+       </button>
+       <p>This is Your Cart</p>
+       <CartContainer/>
+     </Dialog>
+  </div>
+  )
 
 }
 
